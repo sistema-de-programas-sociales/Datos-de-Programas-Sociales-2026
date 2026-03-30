18066008be999a7a2db74619612e731eb8ad1df3
sistema-de-programas-sociales <juanpsalazar2000@gmail.com>
2026-03-30 10:32:22 -0600

    Eliminar carpeta 25-03 y agregar a gitignore

--- 25-03/ACTUALIZAR_DASHBOARD.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Actualizar Dashboard — SDHyBC 2026
-echo.
-echo  Actualizando dashboard...
-echo.
-node "%~dp0GENERAR_REPORTE.js" --web
--- 25-03/GENERAR_REPORTE_Windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Generador de Reporte Quincenal — Chihuahua
-echo.
-echo  Iniciando generador de reporte...
-echo.
-node "%~dp0GENERAR_REPORTE.js" %*
